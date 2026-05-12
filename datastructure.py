054e8cfbd9f5cfb6b215e293211180a9171b6553
Dr Code <proncode@gmail.com>
2026-05-12 11:38:38 +0100

    data structure for list

---
+++ datastructure.py
@@ -0,0 +1,15 @@
+# ::: List :::
+fruits = ['apple','orange','mango','banana','cashew']
+
+# get a fruit
+# print(fruits[0])
+# rename a fruit
+# fruits[0] = 'grape'
+# fruits.append('pineapple')
+# fruits.remove('cashew')
+# fruits.insert(1,'coconut')
+# fruits.pop(3)
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits)
