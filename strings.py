00c8d92e51792542878431ea2974701806cf2c5e
Dr Code <proncode@gmail.com>
2026-05-12 11:29:50 +0100

    string formating and methods

---
+++ strings.py
@@ -0,0 +1,16 @@
+string_one = "Hello world"
+# string formating
+print(f"{string_one}")
+#press ctr + x , after press y, then hit ENTER,
+
+#string methods, methods always ends with (), a method is also known as a function
+
+# print(string_one.capitalize())
+# print(string_one.upper())
+# print(string_one.swapcase())
+# print(string_one.replace("Hello","wole"))
+# print(string_one.split()[0])
+# print(string_one.strip())
+# print(string_one.find('o'))
+# print(string_one.count('o'))
+# print(string_one.isalpha())
