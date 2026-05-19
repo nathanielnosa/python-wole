678211d404d57e9dfd745e3f27912b523577501f
Dr Code <proncode@gmail.com>
2026-05-19 11:44:46 +0100

    finish data structure

--- datastructure.py
+++ datastructure.py
@@ -1,5 +1,5 @@
 # ::: List :::
-fruits = ['apple','orange','mango','banana','cashew']
+# fruits = ['apple','orange','mango','banana','cashew']
 
 # get a fruit
 # print(fruits[0])
@@ -13,3 +13,85 @@ fruits = ['apple','orange','mango','banana','cashew']
 # fruits.reverse()
 # fruits.clear()
 # print(fruits)
+
+# ::: Tuple :::
+# numbers = (1,2,3,4,5) 
+# fruits = ('apple','orange','mango','banana','cashew')
+# sizes = (
+#     ('s','sm'),
+#     ('m','mm'),
+#     ('l','lg'),
+# )
+
+# menu = (
+#     ('Rice',500),
+#     ('Beans',200),
+#     ('Yam',800),
+# )
+
+# print(numbers)
+# print(type(numbers))
+# get a number
+# print(fruits[2])
+# print(len(fruits))
+# del fruits
+
+# print(sizes)
+# print(menu)
+
+# ::: set :::
+# fruits = {'apple', 'orange', 'mango', 'banana', 'cashew'}
+# print(type(fruits))
+# print(fruits)
+# print('apple' in fruits)
+# fruits.add('grape')
+# fruits.remove('apple')
+# fruits.update({'abcd','efghc'})
+# fruits.update(['grape', 'coconut', 'melons'])
+# fruits.clear()
+# print(fruits)
+
+# ::: Dictionary :::
+# student = {
+#     'name': 'Kendall',
+#     'age': 20,
+#     'courses': ['Math', 'CompSci'],
+#     'is_alive': True
+# }
+# print(type(student))
+# print(student)
+# print(student.keys()) #list []
+# print(student.values()) #list []
+# print(student.items()) #[()]
+# print(student['name'])
+# print(student.get('name')) # safer than 
+# student['name'] = 'Kendall Jenner'
+
+# student_new = student.copy()
+# print(student_new)
+# print(len(student))
+# student.update({'name': 'Kendall Jenner', 'age': 21, 'phone': '555-5555'})
+# print(student)
+
+
+# ::: List of Dictionary :::
+todo_app = [
+    {   'id' : 1,
+        'task': 'Go to the gym',
+        'status': 'pending'
+    },
+    {
+       'id' : 2,
+        'task': 'Buy groceries',
+        'status': 'completed'
+    },
+    {
+       'id' : 3,
+        'task': 'Clean the house',
+        'status': 'pending'
+    }
+]
+
+# print(todo_app)
+# print(todo_app[2])
+# print(todo_app[1]['task'])
---
+++ foodapp.py
@@ -0,0 +1,14 @@
+# CREATE A FOOD APP THAT ALLOWS USERS TO SEE THE MENU, ALSO USER CAN SELECT A MENU
+# AND ALSO THE QUANTITY, AND GETS IS ORDER DETAILS AND TOTAL BILL
+
+menu = {
+    1 : ('Rice', 500),
+    2 : ('Beans', 200),
+    3 : ('Yam', 800),
+    4 : ('Fish', 1500),
+    5 : ('Chicken', 1200)
+}
+
+# print(menu)
+# for keys,values in menu.items():
+#     print(f'{keys} : {values[0]} - #{values[1]}')
